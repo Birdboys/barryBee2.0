cde2c8ca886b7012e71937578f6f4ca055ea538f
Birdboys <colbyjbird@comcast.net>
2024-03-22 00:24:45 -0600

    added pollen burst and sweep attack

--- Scripts/base_boss.gd
+++ Scripts/base_boss.gd
@@ -3,7 +3,7 @@ extends Node3D
 @onready var pawLeft := $bossPawLeft
 @onready var bossAnim := $bossAnim
 @onready var bee_follow_speed := 50
-@onready var attacks = ['anticipation1']
+@onready var attacks = ['anticipation4']
 @onready var field_radius := 15.0
 @export var bee_angle := 0.0
 @export var bee_pos : Vector3
@@ -12,7 +12,8 @@ func _ready():
 	
 func _process(delta):
 	if bossAnim.current_animation == "idle" or "anticipation"  in bossAnim.current_animation:
-		rotation.y = rotate_toward(rotation.y, bee_angle, deg_to_rad(bee_follow_speed)*delta)
+		if rad_to_deg(abs(angle_difference(rotation.y, bee_angle))) > 30:
+			rotation.y = bee_angle - deg_to_rad(30) * sign(angle_difference(rotation.y, bee_angle))
 
 func _on_attack_timer_timeout():
 	var current_attack = attacks.pick_random()
@@ -29,11 +30,23 @@ func startAttack(attack: String):
 			var key_id_4 = attackAnim.track_find_key(track_id, 2)
 			var point1 = Vector3.FORWARD.rotated(Vector3.UP, bee_angle) * field_radius + Vector3.UP * 20
 			var point2 = Vector3.FORWARD.rotated(Vector3.UP, bee_angle) * field_radius
-			#print(point1, point2, pawLeft.to_local(point1), pawLeft.to_local(point2))
 			attackAnim.track_set_key_value(track_id, key_id_1, to_local(point1))
 			attackAnim.track_set_key_value(track_id, key_id_2, to_local(point1))
 			attackAnim.track_set_key_value(track_id, key_id_3, to_local(point2))
 			attackAnim.track_set_key_value(track_id, key_id_4, to_local(point2))
+		"attack4":
+			var track_id = attackAnim.find_track("bossPawLeft:position", Animation.TYPE_VALUE)
+			var key_id_1 = attackAnim.track_find_key(track_id, 0.75)
+			var key_id_2 = attackAnim.track_find_key(track_id, 1.5)
+			var key_id_3 = attackAnim.track_find_key(track_id, 2)
+			var key_id_4 = attackAnim.track_find_key(track_id, 2.5)
+			var point1 = Vector3.FORWARD.rotated(Vector3.UP, bee_angle-deg_to_rad(30)) * field_radius
+			var point2 = Vector3.FORWARD.rotated(Vector3.UP, bee_angle) * field_radius
+			var point3 = Vector3.FORWARD.rotated(Vector3.UP, bee_angle+deg_to_rad(30)) * field_radius
+			attackAnim.track_set_key_value(track_id, key_id_1, to_local(point1))
+			attackAnim.track_set_key_value(track_id, key_id_2, to_local(point1))
+			attackAnim.track_set_key_value(track_id, key_id_3, to_local(point2))
+			attackAnim.track_set_key_value(track_id, key_id_4, to_local(point3))
 	bossAnim.play(attack)
 
 func startRecovery(recovery: String):
--- Scripts/bee_character.gd
+++ Scripts/bee_character.gd
@@ -1,6 +1,7 @@
 extends CharacterBody3D
 
 @onready var gravity := 10.0
+@onready var bursting := false
 @onready var bee_speed := 0.5
 @onready var bee_max_vert := 15.0
 @onready var field_height := 5.0
@@ -9,9 +10,14 @@ extends CharacterBody3D
 @onready var pollen_charge_rate := 35.0
 @onready var pollen_use_rate := 20.0
 @onready var pollen_val := 0.0
-@onready var pollen_thrust := gravity + 5.0
+@onready var pollen_thrust := gravity + 7.5
+@onready var pollen_burst_cost := 30.0
+@onready var pollen_burst_thrust := 30.0
+@onready var double_tap_timer := 0.0
+@onready var double_tap_sensitivity := 0.2
 @onready var cam := $beeCam
 @onready var pollenLabel := $beeUI/beeUIMargin/beePollenLabel
+
 func _process(delta):
 	handleInput(delta)
 	handleFlying(delta)
@@ -23,9 +29,9 @@ func handleInput(delta):
 		position = position.rotated(Vector3.UP, -bee_speed*delta)
 	if Input.is_action_pressed("right"):
 		position = position.rotated(Vector3.UP, bee_speed*delta)
-
+	
 func handleFlying(delta):
-	if not is_on_floor():
+	if not is_on_floor() and not bursting:
 		velocity.y -= gravity * delta
 	move_and_slide()
 
@@ -33,14 +39,27 @@ func handlePollen(delta):
 	if is_on_floor():
 		pollen_val += pollen_charge_rate * delta
 		pollen_val = min(pollen_val, 100)
-	if Input.is_action_pressed("fly"):
+		bursting = false
+	if Input.is_action_just_pressed("fly") and not bursting:
+		var new_time = Time.get_ticks_msec()
+		if new_time - double_tap_timer < double_tap_sensitivity * 1000:
+			pollenBurst()
+		double_tap_timer = new_time
+	if Input.is_action_pressed("fly") and not bursting:
 		if pollen_val > 0:
 			pollen_val -= pollen_use_rate * delta
 			pollen_val = max(pollen_val, 0.0)
 			velocity.y += pollen_thrust * delta
 			velocity.y = min(velocity.y, bee_max_vert)
 	pollenLabel.text = "Honey %s" % pollen_val
-	
+
+func pollenBurst():
+	if pollen_val > 0 and not is_on_floor():
+		pollen_val -= pollen_burst_cost
+		pollen_val = max(pollen_val, 0)
+		velocity.y = -pollen_burst_thrust
+		bursting = true
+		
 func moveCamera():
 	cam.position = cam_offset.rotated(Vector3.UP, getAngle()) - Vector3(0, position.y, 0)
 	cam.look_at(Vector3(0, cam_offset.y, 0))
